a390d5939e1a56246390f7c4fa5d11dcc31d760e
ReactJS <meetmistry7007@gmail.com>
2024-02-28 08:15:34 +0530

    0103.2024_Props

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React -d s 
+          Learn React props
         </a>
       </header>
     </div>
