53877594066a3200b7398c5611e4e83c6267683f
ReactJS <meetmistry7007@gmail.com>
2024-02-29 09:30:15 +0530

    Components29

--- src/user/components/Country.js
+++ src/user/components/Country.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 
+// state : It Is an object that store information / date of perticular components
+//   //shotcut con
+
 export default class Country extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "India",
+      timeZone: "GMT+05:30",
+    };
+  }
+
+  changeCountry = () => {
+    this.setState({
+      name: "USA",
+      timeZone: "GMT-5",
+    });
+  };
+
   render() {
     return (
       <>
         <div>Country</div>
         <h1>My Country Is India</h1>
+        <h2>My Country Name Is: {this.state.name}</h2>
+        <h3>My Time Zone Is: {this.state.timeZone}</h3>
+        <button onClick={this.changeCountry}>Change Country</button>
       </>
     );
   }
