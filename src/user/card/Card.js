510e2cbe94977ce303f347ed9d14c8949d0f52dd
ReactJS <meetmistry7007@gmail.com>
2024-03-04 08:21:08 +0530

    04.03.2024css

--- src/user/card/Card.js
+++ src/user/card/Card.js
@@ -1,12 +1,16 @@
 import React from "react";
+import style from "../card/Card"
 
 function Card ({ data }) {
     console.log(data);
     
     return (
         <>
+        <div className={style.Card}  >
             <h1>{data.name}</h1>
             <h3>{data.price}</h3>
+        </div>
+            
         </>
     )
 }
