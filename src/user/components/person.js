e0f20cff248d54efd90ec3daa670db11739e1608
ReactJS <meetmistry7007@gmail.com>
2024-03-08 08:06:52 +0530

    homeWork

--- src/user/components/person.js
+++ src/user/components/person.js
@@ -17,3 +17,4 @@ class Person extends Component {
 }
 
 export default Person;
+
--- src/user/containers/Prosucts_list/Products.js
+++ src/user/containers/Prosucts_list/Products.js
@@ -12,7 +12,9 @@ import {
 
 function Products() {
   const [productData, setPorductData] = useState([]);
-  const [searchTerm, setSearch] = useState("");
+  const [search, setSearch] = useState("");
+  const [sort, setShort] = useState("");
+  const [cetegory,setCetegory] = useState([])
 
   useEffect(() => {
     getData();
@@ -26,13 +28,44 @@ function Products() {
     setPorductData(data);
   };
 
-  const handleSearch = (event) => {
-    setSearch(event.target.value);
+  // const handleSearch = (event) => {
+  //   setSearch(event.target.value);
+  // };
+
+  const handleSearch = () => {
+    let fData = productData.filter(
+      (v) =>
+        v.title.toLowerCase().includes(search) ||
+        v.description.toLowerCase().includes(search) ||
+        v.price.toString().includes(search)
+    );
+
+// if (cetegory) {
+  
+//   fData.filter((v) => (
+//     v.cetegory === cetegory
+//   ))
+// }
+
+    // <h3>{}</h3>
+
+    fData = fData.sort((a, b) => {
+      if (sort === "lh") {
+        return a.price - b.price;
+      } else if (sort === "hl") {
+        return b.price - a.price;
+      } else if (sort === "az") {
+        return a.title.localeCompare(b.title);
+      } else if (sort === "za") {
+        return b.title.localeCompare(a.title);
+      }
+    }); 
+
+    return fData;
   };
 
-  const searchProducts = productData.filter((product) =>
-    product.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const finalData = handleSearch();
+  console.log(finalData);
 
   return (
     <div className="container">
@@ -43,37 +76,55 @@ function Products() {
             <input
               type="text"
               placeholder="Search"
-              value={searchTerm}
-              onChange={handleSearch}
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}
             />
+
+            <select onChange={(event) => setShort(event.target.value)}>
+              <option value="0">--Select Cetegory--</option>
+              <option value="lh">Price: Low To High</option>
+              <option value="hl">Price: High To Low</option>
+              <option value="az">Product: A -Z</option>
+              <option value="za">Product: Z -A</option>
+            </select>
+
+            <br></br>
+
+            <br></br>
+                        
+            {/* <button value={cetegory} onChange={(event) => setCetegory(event.target.value)}>cetegory</button> */}
+
           </div>
 
-          {searchProducts.map((v) => (
-            <div className="col-md-4 gy-4">
+          {finalData.map((v) => (
+            <div class="col-md-4 gy-4">
               {/* <img src={v.image} height={"150px"} width={"150px"} /> */}
               {/* <h2>{v.price}</h2> */}
               {/* <h3>{v.title}</h3> */}
 
-              <Card
-                style={{
-                  width: "18rem",
-                }}
-              >
-                <img
-                  alt="Sample"
-                  src={v.image}
-                  height={"250px"}
-                  width={"250px"}
-                />
-                <CardBody>
-                  <CardTitle tag="h5">{v.title.substring(0, 20)}</CardTitle>
-                  <CardSubtitle className="mb-2 text-muted" tag="h6">
-                    {v.description.substring(0, 50)}
-                  </CardSubtitle>
-                  <CardText>{v.price}</CardText>
-                  <Button>Add To Cart</Button>
-                </CardBody>
-              </Card>
+              <div class="box">
+                <Card
+                  style={{
+                    width: "18rem",
+                  }}
+                >
+                  <img
+                    alt="Sample"
+                    src={v.image}
+                    height={"250px"}
+                    width={"250px"}
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">{v.title.substring(0, 20)}</CardTitle>
+                    <CardSubtitle class="mb-2 text-muted" tag="h6">
+                      {v.description.substring(0, 50)}
+                      ...
+                    </CardSubtitle>
+                    <CardText>{v.price}</CardText>
+                    <Button>Add To Cart</Button>
+                  </CardBody>
+                </Card>
+              </div>
             </div>
           ))}
         </div>
